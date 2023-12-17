7f68cf8a2182187704f0c034c325cf3e59628946
Nitish kumar <krnitish540@gmail.com>
2023-12-17 14:45:28 +0530

    Intreget add transiction in app

--- client/src/view/Transaction/Transaction.js
+++ client/src/view/Transaction/Transaction.js
@@ -1,10 +1,109 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import Navbar from './../../component/Navbar/Navbar'
+import './Transaction.css'
+import axios from 'axios'
 function Transaction() {
+  const [amount,setAmount]=useState()
+  const [note,setNote]=useState('')
+  const [category,setCategory]=useState('')
+  const [type,setType]=useState('')
+  const [userdata,setUserData]=useState({})
+
+  useEffect(()=>{
+    const data=JSON.parse(localStorage.getItem('user')||"{}")
+    setUserData(data)
+  },[])
+
+  const clearInputFields=()=>{
+    setAmount("");
+    setNote("");
+    setCategory("");
+    setType("");
+  }
+  
+  const addTransaction=async()=>{
+    try{
+      const responce=await axios.post('/api/transaction',{
+        user:userdata._id,
+        amount:amount,
+        decsription:note,
+        category:category,
+        type:type
+      })
+      if(responce?.data?.success===true){
+         alert(responce?.data?.message)
+         clearInputFields()
+      }else{
+        alert(responce?.data?.message)
+      }
+    }catch(err){
+      console.log(err.message)
+    }
+  }
+
+
   return (
-    <div>
-      
-    </div>
+    <>
+      <Navbar/>
+      <p style={{textAlign:'center'}}>Welcome To Budget Management And Expence Tracker System</p>
+      <div className='main-transaction-div'>
+       
+          <div className='sub-transaction-div'>
+
+          </div>
+
+
+          <div className='sub-transaction-div'>
+<p style={{textAlign:'center'}}>Add Transaction</p>
+
+          <input type='text'
+          value={amount}
+          placeholder='Amount'
+          className='input-box'
+          onChange={(e) => {
+            setAmount(e.target.value)
+          }} />
+
+        <input type='text'
+          value={note}
+          placeholder='Add Note'
+          className='input-box'
+          onChange={(e) => {
+            setNote(e.target.value)
+          }} />
+          <div className='input-contanier'> 
+          <select className='select-type'
+          value={category}
+          onChange={(e) => {
+            setCategory(e.target.value)
+          }}>
+          <option>Select Category</option>
+          <option value={"food"}>Food</option>
+          <option value={"entertainment"}>Entertainment</option>
+          <option value={"education"}>Education</option>
+          <option value={"transportation"}>Tavell</option>
+          <option value={"salary"}>Salary</option>
+          <option value={"gift"}>Business</option>
+          <option value={"shopping"}>Shopping</option>
+          <option value={"rent"}>Rent</option>
+          <option value={"other"}>Other</option>
+        </select>
+
+        <select className='select-type'
+          value={type}
+          onChange={(e) => {
+            setType(e.target.value)
+          }}>
+          <option>Select Type</option>
+          <option value={"credit"}>Credit</option>
+          <option value={"debit"}>Debit</option>
+        </select>
+       
+          </div>
+                 <button type='button' onClick={addTransaction}>Add Transaction</button>
+         </div>
+      </div>
+    </>
   )
 }
 
